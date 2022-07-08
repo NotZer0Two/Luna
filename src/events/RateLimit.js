7aacf171dc5b69cf79293edc032eb92bb3a200de
NotZer0Two <lyonfan2010@gmail.com>
2022-07-08 06:42:35 +0200

    Majour issues about ratelimiting

--- src/events/RateLimit.js
+++ src/events/RateLimit.js
@@ -9,8 +9,6 @@ client.on('rateLimit', (info) => {
      const MINUTES = Math.floor((info.timeout % 3600000) / 60000);
      const SECONDS = Math.floor(((info.timeout % 3600000) % 60000) / 1000);
 
-     if(MINUTES => 0) return;
-
      console.log(
        `Rate limit exceeded. rate limited for ${HOURS} hours, ${MINUTES} minutes, and ${SECONDS} seconds.
    
@@ -23,23 +21,5 @@ client.on('rateLimit', (info) => {
 
      if(info.global) {
         console.log("/!\\ Can't contact the webhook for the rate limit /!\\")
-     } else {
-        if(MINUTES => 0) return;
-
-        const embed = new MessageEmbed()
-            .setTitle("📬 Rate Limit")
-            .setColor("#a1131d")
-            .setDescription(`Important Rate limit information.`)
-            .addField("ℹ️ Rate Limit", `${info.global ? "Global" : "Server"}`)
-            .addField("ℹ️ Method", `${info.method}`)
-            .addField("🖥️ Path", `${info.path}`)
-            .addField("🖥️ Route", `${info.route}`)
-            .addField("🕰️ Timeout", `${HOURS} hours, ${MINUTES} minutes, and ${SECONDS} seconds.`)
-            .setTimestamp();
-
-        client.stafflogs.send({
-            username: 'Rate Limit',
-            embeds: [embed],
-        });
      }
    });
